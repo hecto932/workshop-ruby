36963aa37faef6cebdbc1f9af7bbae0eff66bbf0
Hector Flores <hecto932@gmail.com>
2016-03-24 16:30:36 -0430

    Numeros

---
+++ numeros.rb
@@ -0,0 +1,16 @@
+# Numeros!!
+# Un numero es un objeto.
+# .next devuelve el siguiente
+# .pred devuelve el anterior
+# .zero es cero ?
+# .odd ? Impar
+# .even ? par
+# .class devuelve la clase
+
+puts 92*12 #Multiplicación
+
+puts 1+2 #suma
+
+puts 3-1 #resta
+
+puts 8/2 #division
