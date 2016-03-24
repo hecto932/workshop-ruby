debeff76f888574830fc42891fe6045ac43fa3e0
Hector Flores <hecto932@gmail.com>
2016-03-24 18:41:54 -0430

    Float, Integer, Fixnum

--- numeros.rb
+++ numeros.rb
@@ -14,3 +14,27 @@ puts 1+2 #suma
 puts 3-1 #resta
 
 puts 8/2 #division
+
+puts (9.0/2.0).class
+
+puts (9.0/2.0).floor
+
+puts (9.0/2.0).ceil
+
+puts (9.0/2.0).next_float
+
+puts (9.0/2.0).prev_float
+
+puts (9.0/2.0).zero?
+
+puts (9.0/2.0).real?
+
+puts (9.0/2.0).to_i
+
+puts 10 + 20 * 10
+
+puts "Hola " + "platzi"
+
+saludo = "hola platzi"
+
+puts saludo * 10
